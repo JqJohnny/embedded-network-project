c62d8032a8d08939fa49ecd1b31f775a3fde4659
Johnny Quach <Johnqdx@gmail.com>
2026-05-09 15:51:15 -0700

    Update device_client.py
    Starting to rewrite the client

--- device_client.py
+++ device_client.py
@@ -1,21 +1,11 @@
-from socket import *
+import socket
+import json
+import random
+import time
 
-serverIP = input('Insert server IP: ')
-serverPort = input('Insert server port: ')
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((serverIP, int(serverPort)))
-print('Connected to server.')
-while True:
-    message = input('Insert a message to be sent to server: ')
-    clientSocket.send(message.encode())
-    if message.upper() == "QUIT":
-        print("Client informing the server that it is quitting.")
-        while True:
-            message = clientSocket.recv(1024).decode()
-            if message.upper() == 'QUIT':
-                clientSocket.close()
-                exit()
-            print(message)
-            returned = 'Message Received.'
-            clientSocket.send(returned.encode())
-    print('Client has sent the message: ', message)
+HOST = '127.0.0.1'
+PORT = 12000
+
+DEVICE_ID = f"router_{random.randint(1,100)}"
+
+print(f"{DEVICE_ID}")
